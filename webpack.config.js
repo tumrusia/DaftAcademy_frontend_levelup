6272ee63f625bbaf402cd6d2265b147cc867c2f5
tumrusia <marta.markocka99@gmail.com>
2020-04-16 18:55:21 +0200

    nowe pliki

--- webpack.config.js
+++ webpack.config.js
@@ -2,39 +2,56 @@ const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-     entry: './start.js',
-     output: {
-         filename: './start.js',
-         path: path.resolve(__dirname, 'dist')
-     },
-     module: {
-         rules: [
-             {
-                 test: /\.js$/,
-                 exclude: /node_modules/,
-                 use: {
-                     loader: 'babel-loader',
-                     options: {
-                         presets: ['@babel/preset-env']
-                     }
-                 }
-             },
-             {
-                 test: /\.css$/,
-                 exclude: /node_modules/,
-                 use: ['sass-loader']
-             },
-             {
-                test: /\.png$/,
-                exclude: /node_modules/,
-                use: ['file-loader']
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|svg|gif|ico|webp|eot|ttf|woff|woff2|pdf|mp4|webm)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'assets',
             },
-         ]
-     },
-     plugins: [
-         new HtmlWebPackPlugin({
-             template: './src/index.html',
-             filename: './index.html'
-         })
-     ]
+          },
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    })
+  ]
 }
